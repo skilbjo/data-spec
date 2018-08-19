4f098065c599dd73f488d62123526c44a1fcc404
John Skilbeck <john.skilbeck@gmail.com>
2018-08-19 15:23:46 -0700

    Add logs table

--- README.md
+++ README.md
@@ -6,3 +6,13 @@ Personal datawarehouse ERD. Applies to these projects:
 - <https://github.com/skilbjo/router-logs>
 
 <img src='dev-resources/img/erd.png' width=900 />
+
+### sample queries
+
+```sql
+select
+  date_parse(s3.datetime,'%d/%b/%Y:%H:%i:%S +%f'),
+  *
+from logs.s3
+limit 10
+```
---
+++ resources/athena/logs.sql
@@ -0,0 +1,40 @@
+drop schema logs cascade;
+
+create schema logs;
+
+drop table if exists logs.s3;
+
+create external table logs.s3 (
+  bucket_owner              string,
+  bucket                    string,
+  datetime                  string,
+  ip                        string,
+  requestor_id              string,
+  arn                       string,
+  request_id                string,
+  operation                 string,
+  key                       string,
+  request_uri_operation     string,
+  request_uri_key           string,
+  http_method               string,
+  http_status               string,
+  error_code                string,
+  bytes_sent                string,
+  object_size               string,
+  total_time                string,
+  turn_around_time          string,
+  referer                   string,
+  user_agent                string,
+  version_id                string
+)
+row format serde
+  'org.apache.hadoop.hive.serde2.RegexSerDe'
+with serdeproperties (
+  'serialization.format' = '1',
+  'input.regex' = '([^ ]*) ([^ ]*) \\[(.*?)\\] ([^ ]*) ([^ ]*:([0-9]+):.*[^ ]) ([^ ]*) ([^ ]*) ([^ ]*) \\\"([^ ]*) ([^ ]*) (- |[^ ]*)\\\" (-|[0-9]*) ([^ ]*) ([^ ]*) ([^ ]*) ([^ ]*) ([^ ]*) ([^ ]*) (\"[^\"]*\") ([^ ]*)$'
+)
+location
+  's3://skilbjo-logs/'
+;
+
+msck repair table logs.s3;
